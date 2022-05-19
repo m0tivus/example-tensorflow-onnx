1b82eea019bf4c0118ae511114141c4aef19a6c6
Pablo Huijse <pablo.huijse@gmail.com>
2022-05-19 15:45:13 -0400

    Specify wasm

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,5 +1,5 @@
 async function main() {
-    const session = await ort.InferenceSession.create("../models/model.onnx");
+    const session = await ort.InferenceSession.create("../models/model.onnx", {executionProviders: ['wasm']});
     const data = Float32Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
     const data_tensor = new ort.Tensor('float32', data, [3, 4]);
     const feeds = { dense_input: data_tensor};
