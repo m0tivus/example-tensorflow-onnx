3c38c3fa871fc9d327b21caef0f3a2ea4f7a9ff0
Pablo Huijse <pablo.huijse@gmail.com>
2022-06-02 18:46:01 -0400

    make invariant to names in model

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,10 +1,17 @@
+//const ort = require('onnxruntime-web');
+
 async function main() {
     const session = await ort.InferenceSession.create("../models/model.onnx", {executionProviders: ['wasm']});
+    const input = new String(session.inputNames[0]);
+    const output = new String(session.outputNames[0]);
     const data = Float32Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
     const data_tensor = new ort.Tensor('float32', data, [3, 4]);
-    const feeds = { dense_input: data_tensor};
+    //console.log(input);
+    const feeds = {[input] : data_tensor};
+    //console.log(feeds);
+    //console.log(output);
     const result = await session.run(feeds);
-    document.write(result.dense_1.data);
+    document.write(result[output].data);
     
 }
 main();
